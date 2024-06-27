8bd13558ab1dfb02b855272abc23223816b2bfd2
wendypam <wendypamelapp@gmail.com>
2024-06-27 07:42:12 -0500

    Inicio

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,14 @@
+
+interface Props extends React.LabelHTMLAttributes<HTMLLabelElement>{
+
+}
+
+
+export function Label({children, ...props}:Props) {
+    return (
+        <label 
+        className="block text-sm font-medium leading-6 text-gray-900"
+        {...props}
+        >{children}</label>
+    )
+}
---
+++ src/components/ui/index.ts
@@ -0,0 +1,9 @@
+import { Input } from './Input';
+import { Label } from './Label';
+import { Button } from './Button';
+
+export {
+    Input,
+    Label,
+    Button
+}
